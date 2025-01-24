ecc1af33b0d4c32e04d9c01ed57460789bafddb9
Özgü Günes <og@codera.at>
2025-01-24 18:10:06 +0100

    Sort articles by year;

--- pages/publications.vue
+++ pages/publications.vue
@@ -2,9 +2,11 @@
   <div class="pt-16">
     <Container class="min-h-screen py-16 sm:py-32">
       <Headline tag="h1" color="secondary" theme="pill">Publications</Headline>
-      <div class="grid grid-cols-1 sm:grid-cols-2 gap-8 mt-16">
-        <div v-for="article in articles">
-          <NuxtLink class="text-lg font-bold" :to="article.link">{{ article.title }}</NuxtLink>
+      <div class="mt-16 grid grid-cols-1 gap-8 sm:grid-cols-2">
+        <div v-for="article in sortedArticles" :key="article.link">
+          <NuxtLink class="text-lg font-bold" :to="article.link">{{
+            article.title
+          }}</NuxtLink>
           <p>{{ article.authors }}</p>
           <p>{{ article.publication }}</p>
           <p>{{ article.year }}</p>
@@ -16,6 +18,10 @@
 
 <script setup>
 import articles from "/api/publications.json";
+
+const sortedArticles = computed(() => {
+  return articles.sort((a, b) => b.year - a.year);
+});
 </script>
 
 <style lang="scss" scoped></style>
